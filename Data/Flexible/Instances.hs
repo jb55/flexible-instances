c6a4685f20ba7730cc6d43435efb7b7070e6b70d
William Casarin <bill@casarin.me>
2016-08-18 14:52:38 -0700

    Release 0.3.0

--- Data/Flexible/Instances.hs
+++ Data/Flexible/Instances.hs
@@ -3,16 +3,20 @@
 module Data.Flexible.Instances (
 ) where
 
-import           Data.Flexible
-import           Data.Text (Text)
+import Control.Applicative
+import Control.Monad.Error
+import Data.Aeson
+import Data.Typeable
+import Data.Default (Default(..))
+import Data.Flexible
+import Data.Text (Text)
+import Data.ByteString.Char8 (unpack)
+import Database.Persist.Class
+import Database.Persist.Sql
+import Database.Persist.Types (PersistValue(..))
+import Database.PostgreSQL.Simple.FromField
+
 import qualified Data.Text as T
-import           Control.Applicative
-import           Data.Default (Default(..))
-import           Database.Persist.Class
-import           Database.Persist.Sql
-import           Database.Persist.Types (PersistValue(..))
-import           Control.Monad.Error
-import           Data.Aeson
 
 instance (ToJSON a, ToJSON b) => ToJSON (Flexible b a) where
   toJSON (Some x) = toJSON x
@@ -23,6 +27,15 @@ instance (FromJSON a, FromJSON b) => FromJSON (Flexible b a) where
   parseJSON Null = return NotThere
   parseJSON v = (Some <$> parseJSON v) <|>
                 (Raw  <$> parseJSON v)
+
+instance (Typeable b, Typeable a, FromField b, FromField a) => FromField (Flexible b a) where
+  fromField _ Nothing   = return NotThere
+  fromField f bs =
+    (Some <$> fromField f bs) <|>
+    (Raw <$> fromField f bs) <|>
+    (returnError ConversionFailed f
+       ("no parse on Some or Raw in Flexible: " ++ maybe "null" unpack bs))
+
 
 -- not sure if this should be here but we need it for the PersistField instance
 instance Error Text where
--- flexible-instances.nix
+++ flexible-instances.nix
@@ -1,12 +1,13 @@
 { mkDerivation, aeson, base, data-default, flexible, mtl
-, persistent, stdenv, text
+, persistent, stdenv, text, postgresql-simple, bytestring
 }:
 mkDerivation {
   pname = "flexible-instances";
   version = "0.2.0";
   src = ./.;
   buildDepends = [
-    aeson base data-default flexible mtl persistent text
+    aeson base data-default flexible mtl persistent text postgresql-simple
+    bytestring
   ];
   description = "Helpful instances for Data.Flexible";
   license = stdenv.lib.licenses.mit;
